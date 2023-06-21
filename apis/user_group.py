409dfa8e3009c97fc7440cc666c6c6d83d1a7b3f
David Akwuru <daakwuru@gmail.com>
2023-06-21 13:31:00 +0100

    created endpoint to update, view all and view single user group per client

--- apis/user_group.py
+++ apis/user_group.py
@@ -25,7 +25,7 @@ async def create_group(request: Request, user_data: UserGroupSchema, db: Session
     return group_crud.create_user_group(db, client_id, user_data, route_name)
 
 @user_group_router.patch("/disable/{user_group_id}", summary="Disable a user group for a client", status_code=200,
-                                                dependencies=[Depends(validate_client_key)], name="user_group_disable")
+                         dependencies=[Depends(validate_client_key)], name="user_group_disable")
 async def disable_group(request:Request, user_group_id: int, db: Session = Depends(get_db)):
     client_id = request.state.data
     # get the name of the route.
@@ -37,7 +37,7 @@ async def disable_group(request:Request, user_group_id: int, db: Session = Depen
     return group_crud.update_group(db, client_id, user_group_id, update_data, route_name)
 
 @user_group_router.patch("/enable/{user_group_id}", summary="Enable a user group for a client", status_code=200,
-                                                dependencies=[Depends(validate_client_key)], name="user_group_enable")
+                         dependencies=[Depends(validate_client_key)], name="user_group_enable")
 async def enable_group(request:Request, user_group_id: int, db: Session = Depends(get_db)):
     client_id = request.state.data
     # get the name of the route.
@@ -47,3 +47,31 @@ async def enable_group(request:Request, user_group_id: int, db: Session = Depend
         "status": True
     }
     return group_crud.update_group(db, client_id, user_group_id, update_data, route_name)
+
+@user_group_router.patch("/update/{user_group_id}", summary="Update a user group data for a client", status_code=200,
+                         dependencies=[Depends(validate_client_key)], name="user_group_update")
+async def disable_group(request:Request, user_group_id: int, update_data: UpdateUserGroupSchema, 
+                        db: Session = Depends(get_db)):
+    client_id = request.state.data
+    # get the name of the route.
+    route_name = request.scope['route'].name
+    return group_crud.update_group_data(db, client_id, user_group_id, update_data, route_name)
+
+@user_group_router.get('/', summary="Get all user groups per client", status_code=200,
+                       dependencies=[Depends(validate_client_key)], name="user_group_all")
+async def view_all(request:Request, page: int=Query(1, ge=1), 
+                   page_size:int =10, db: Session = Depends(get_db)):
+    client_id = request.state.data
+    # get the name of the router.
+    route_name = request.scope['route'].name
+    
+    return group_crud.get_groups(db, client_id, page, page_size, route_name)
+
+@user_group_router.get('/single/{group_id}', summary="View Single User Group", status_code=200,
+                       dependencies=[Depends(validate_client_key)], name="user_group_single")
+async def view_single(request:Request, group_id: int, db:Session = Depends(get_db)):
+    client_id = request.state.data
+    # get the name of the router
+    route_name = request.scope['route'].name
+    
+    return group_crud.single_group(db, client_id, group_id, route_name)
--- crud/group_crud.py
+++ crud/group_crud.py
@@ -21,19 +21,38 @@ from datetime import datetime
 
 db = Session()
 
+def check_group_slug(db, client_id, user_data):
+    get_slug = models.UserGroup.user_group_object(db).filter_by(
+            client_id=client_id, slug= user_data.slug).first()
+    get_name = models.UserGroup.user_group_object(db).filter_by(
+        client_id=client_id, group_name= user_data.group_name).first()
+    
+    if get_slug is not None:
+        return False, f"Sorry, User Group with slug name {user_data.slug} exists for this client"
+    if get_name is not None:
+        return False, f"Sorry, User Group with group name {user_data.group_name} exists for this client"
+    
+    return True, ""
+
+def check_client_id(db, user_group_id, client_id):
+    # first check if the user_id exists for that client.
+    get_user_id = models.UserGroup.get_user_group_by_id(db, user_group_id)
+    if get_user_id is None:
+        return False, "No User Group with such ID"
+    
+    if get_user_id.client_id != client_id:
+        return False, "Group ID doesn't belong to client"
+    
+    return True, ""
+
+
 def create_user_group(db, client_id, user_data, route_name):
     # first check if the client is already present.
     try:
         # check if the slug and group name exists for the client.
-        get_slug = models.UserGroup.user_group_object(db).filter_by(
-            client_id=client_id, slug= user_data.slug).first()
-        get_name = models.UserGroup.user_group_object(db).filter_by(
-            client_id=client_id, group_name= user_data.group_name).first()
-        
-        if get_slug is not None:
-            return exceptions.bad_request_error(f"Sorry, User Group with slug name {user_data.slug} exists for this client")
-        if get_name is not None:
-            return exceptions.bad_request_error(f"Sorry, User Group with group name {user_data.group_name} exists for this client")
+        bool_result, message = check_group_slug(db, client_id, user_data)
+        if not bool_result:
+            return exceptions.bad_request_error(message)
         # convert this to a dictionary
         user_dict = user_data.dict(exclude_unset=True)
         user_dict['client_id'] = client_id
@@ -53,12 +72,9 @@ def create_user_group(db, client_id, user_data, route_name):
 def update_group(db, client_id, user_group_id, update_data, route_name):
     try:
         # first check if the user_id exists for that client.
-        get_user_id = models.UserGroup.get_user_group_by_id(db, user_group_id)
-        if get_user_id is None:
-            return exceptions.bad_request_error("No User Group with such ID")
-        
-        if get_user_id.client_id != client_id:
-            return exceptions.bad_request_error("Group ID doesn't belong to client")
+        bool_result, message = check_client_id(db, user_group_id, client_id)
+        if not bool_result: 
+            return exceptions.bad_request_error(message)
         # update right away.
         updated_group = models.UserGroup.update_user_group(db, user_group_id, update_data)
         if not updated_group:
@@ -72,3 +88,48 @@ def update_group(db, client_id, user_group_id, update_data, route_name):
         return exceptions.server_error(detail=str(e))
     
     return success_response.success_message(updated_group, "User Group was successfully updated")
+
+def update_group_data(db, client_id, user_group_id, update_data, route_name):
+    """Update data of an existing client group"""
+    # check if the slug and group name exists for the client.
+    bool_result, message = check_group_slug(db, client_id, update_data)
+    if not bool_result:
+        return exceptions.bad_request_error(message)
+    
+    user_dict = update_data.dict(exclude_unset=True)
+
+    return update_group(db, client_id, user_group_id, user_dict, route_name)
+
+def get_groups(db, client_id, page, page_size, route_name):
+    try:
+        client_group = models.UserGroup.get_client_user_groups(db, client_id).options(
+            load_only('id'),
+            load_only('slug'),
+            load_only('group_name'),
+            load_only('status')
+        )
+        
+        #calculate the offset.
+        page_offset = Params(page=page, size=page_size)
+        data_result = paginate(client_group, page_offset)
+    except Exception as e:
+        return exceptions.server_error(detail=str(e))
+    
+    return success_response.success_message(data_result)
+
+def single_group(db, client_id, group_id, route_name):
+    try:
+        # first check if the user_id exists for that client.
+        bool_result, message = check_client_id(db, group_id, client_id)
+        if not bool_result: 
+            return exceptions.bad_request_error(message)
+        
+        data_result = models.UserGroup.user_group_object(db).options(
+            load_only('slug'),
+            load_only('group_name'),
+            load_only('status')  
+        ).get(group_id)
+    except Exception as e:
+        return exceptions.server_error(detail=str(e))
+    
+    return success_response.success_message(data_result)
