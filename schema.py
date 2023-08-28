547269e20e3d9e8b13c63246111da8387f143379
David Akwuru <daakwuru@gmail.com>
2023-08-28 08:45:21 +0100

    .

--- schema.py
+++ schema.py
@@ -97,6 +97,7 @@ class PermissionSchema(BaseModel):
     
 class UpdatePermissionSchema(BaseModel):
     description: str = None
+    label: str = None
     
 class UserPermissionSchema(BaseModel):
     router_name: str
